660cef1faae05f631538a96e92ff7285d7300c6f
Ajay <ajaychandru20@gmail.com>
2024-11-04 23:44:40 +0530

    done user & pass testing

---
+++ Section02_FirstSeleniumProject/src/main/java/EnterDetails.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EnterDetails {
+
+      public static void main(String[] arg){
+
+          WebDriver driver = new ChromeDriver();
+          driver.get("https://practicetestautomation.com/practice-test-login/");
+
+          WebElement username = driver.findElement(By.id("username"));
+          username.sendKeys("student");
+          WebElement password = driver.findElement(By.id("password"));
+          password.sendKeys("Password123");
+
+          WebElement submit = driver.findElement(By.id("submit"));
+          submit.click();
+
+          driver.quit();
+
+
+
+
+      }
+
+
+}
